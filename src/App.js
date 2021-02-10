8c4e9b994216370d0481abb98e5b8271ffbba077
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-02-10 17:36:31 -0600

    feat: add pacient manager project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
+
+  /**
+ * Citas en local storage
+ * solo almacena string se tiene que parcear con json
+ */
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  /**
+   * Arreglo con citas a mostar en el listado
+   */
+  const [citas, guardarCitas] = useState(citasIniciales);
+  /**
+   * Función que toma la cita actual y toma la nueva
+  */
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ]);
+  };
+
+  /**
+   * Función que elimina una cita por su id
+   * filtra el que es igual y lo guarda en un arreglo,
+   * 1, 2, 3 => eliminaria el 1 y el 2
+   * por eso se busca el diferente al id guardaria en el arreglo los diferentes
+   */
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  /**
+   * Mensaje cuando no hay citas
+   */
+  const titulo = citas.length === 0 ? 'No hay Citas' : 'Administra tus Citas'
+
+  /**
+   * Cambios en Citas
+   */
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+    
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            <h1>{titulo}</h1>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
   );
 }
 
